a1f953ec75078fe3c5d93c89c53208d04ff0b604
Park18 <sowooni35@naver.com>
2020-02-13 21:19:15 +0900

    Change extension Sync.h -> Sync.hpp

---
+++ Sync.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef SYNC_HPP
+#define SYNC_HPP
+
+#include <boost/filesystem.hpp>
+
+/**
+ * @brief 동기화를 담당하는 클래스
+ * @details 동기화
+ * @author Park18
+ * @date 2020-02-13
+ * @version 0.1.1
+ */
+class Sync
+{
+private:
+
+};
+	
+#endif // !SYNC_HPP
