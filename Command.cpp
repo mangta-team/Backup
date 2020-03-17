4063d1df9b3a8a4d55b1ba5f44530ba566009dcd
Park18 <sowooni35@naver.com>
2020-03-17 22:30:21 +0900

    EDIT Command.hpp
    delete exe_only method
     - command
     - get_work
     - get_root_path
     - get_dest_path
     - is_right

--- Command.cpp
+++ Command.cpp
@@ -2,52 +2,6 @@
 
 using namespace std;	
 
-void backup::command::command(const std::string command)
-{
-	if (!backup::command::is_right(command))
-	{
-		cout << "[알림] >> 잘못된 명령어 입니다." << endl << endl;
-		return;
-	}
-
-	switch (get_work(command))
-	{
-	case backup::command::work::add :
-		backup::dir_center::add(backup::command::get_root_path(command ,backup::command::path_option::command), 
-			backup::command::get_dest_path(command, backup::command::path_option::command));
-		break;
-
-	case backup::command::work::_delete :
-		backup::dir_center::_delete();
-		break;
-
-	case backup::command::work::print :
-		backup::dir_center::print();
-		break;
-
-	case backup::command::work::sync :
-		cout << "[알림] >> 다음 항목을 동기화 합니다." << endl << endl;
-
-		{
-			vector<string> paths; 
-			if (backup::dir_center::print(&paths))
-				return;
-		
-			for (int index = 0; index < paths.size(); index++)
-				backup::sync::sync(backup::command::get_root_path(paths[index], backup::command::path_option::file),
-					backup::command::get_dest_path(paths[index], backup::command::path_option::file));
-		}
-
-		cout << "[알림] >> 동기화가 끝났습니다." << endl << endl;
-
-		break;
-
-	case backup::command::work::help :
-		// 
-		break;
-	}
-}
-
 void backup::command::command(std::vector<std::string> vector_argv)
 {
 	backup::system::Error_Code error_code;
@@ -94,52 +48,6 @@ void backup::command::command(std::vector<std::string> vector_argv)
 	}
 }
 
-backup::command::work backup::command::get_work(const std::string command)
-{
-	string work = command.substr(0, command.find(COMMAND_TOKEN));
-
-	if (work == "add")
-		return backup::command::work::add;
-
-	else if (work == "delete")
-		return backup::command::work::_delete;
-
-	else if (work == "print")
-		return backup::command::work::print;
-
-	else if (work == "sync")
-		return backup::command::work::sync;
-
-	else if (work == "help")
-		return backup::command::work::help;
-
-	else
-		return backup::command::work::unknown;
-}
-
-backup::command::work backup::command::get_work(const std::string command, std::string* const str_work)
-{
-	string work = command.substr(0, command.find(COMMAND_TOKEN));
-	*str_work = work;
-
-	if (work == "add")
-		return backup::command::work::add;	
-
-	else if (work == "delete")
-		return backup::command::work::_delete;
-
-	else if (work == "print")
-		return backup::command::work::print;
-
-	else if (work == "sync")
-		return backup::command::work::sync;
-
-	else if (work == "help")
-		return backup::command::work::help;
-
-	else
-		return backup::command::work::unknown;
-}
 
 backup::command::work backup::command::get_work(std::vector<std::string> vector_argv)
 {
@@ -164,24 +72,6 @@ backup::command::work backup::command::get_work(std::vector<std::string> vector_
 		return backup::command::work::unknown;
 }
 
-bfs::path backup::command::get_root_path(const std::string command, backup::command::path_option path_option)
-{
-	bfs::path root;
-
-	if (path_option == backup::command::path_option::file)
-		root = command.substr(0, command.find('-'));
-	
-	else
-	{
-		string str_root = command.substr(command.find("-root"), command.find("-dest") - command.find("-root"));
-		str_root = str_root.substr(str_root.find_first_of('\"') + 1, 
-			str_root.find_last_of('\"') - str_root.find_first_of('\"') - 1);
-		root = str_root;
-	}
-
-	return root.generic_string();
-}
-
 bfs::path backup::command::get_root_path(std::vector<std::string> vector_argv)
 {
 	int index = 0;
@@ -194,24 +84,6 @@ bfs::path backup::command::get_root_path(std::vector<std::string> vector_argv)
 
 	bfs::path root(vector_argv[index]);
 	return root.generic_string();
-}
-
-bfs::path backup::command::get_dest_path(const std::string command, backup::command::path_option path_option)
-{
-	bfs::path dest;
-
-	if (path_option == backup::command::path_option::file)
-		dest = command.substr(command.find('-') + 1, command.size() - command.find("-dest") - 1);
-
-	else
-	{
-		string str_dest = command.substr(command.find("-dest"), command.size() - command.find("-dest"));
-		str_dest=str_dest.substr(str_dest.find_first_of('\"') + 1,
-			str_dest.find_last_of('\"') - str_dest.find_first_of('\"') - 1);
-		dest = str_dest;
-	}
-
-	return dest.generic_string();
 }
 
 bfs::path backup::command::get_dest_path(std::vector<std::string> vector_argv)
@@ -228,44 +100,6 @@ bfs::path backup::command::get_dest_path(std::vector<std::string> vector_argv)
 	return dest.generic_string();
 }
 
-bool backup::command::is_right(const std::string command)
-{
-	string str_work;
-
-	// work이 unknown일 때
-	// 오류 문장이다.
-	if (backup::command::get_work(command) == backup::command::work::unknown)
-		return false;
-
-
-	// work이 backup::command::work::add 이 아닐 때(옵션이 필요하지 않는 work)
-	// 명령어와 work의 길이가 같으면 옳바른 문장,
-	// 아니면 오류 문장이다.
-	else if (backup::command::get_work(command, &str_work) != backup::command::work::add)
-	{		
-		if (str_work.size() == command.size())
-			return true;
-
-		return false;
-	}
-
-	// backup::command::work::add 일 때
-	else
-	{
-		// "-root"의 위치가 "-dest"의 위치보다 높으면 
-		// 오류 문장이다.
-		if (command.find("-root") > command.find("-dest"))
-			return false;
-
-		// root와 dest의 경로가 존재하면 옳바른 문장,
-		// 존재하지 않으면 오류 문장이다.
-		if (bfs::exists(backup::command::get_root_path(command, backup::command::path_option::command)) &&
-			bfs::exists(backup::command::get_dest_path(command, backup::command::path_option::command)))
-			return true;
-
-		return false;
-	}
-}
 
 bool backup::command::is_right(std::vector<std::string> vector_argv, backup::system::Error_Code& error_code)
 {
--- Command.hpp
+++ Command.hpp
@@ -34,31 +34,12 @@ enum class path_option
     command,
 };
 
-/**
- * @brief
- * @param string command 명령어
- */
- void command(const std::string command);
-
  /**
  * @brief
  * @param cosnt int argc 토큰(' ')으로 나눠진 명령어의 개수
  * @param const char* 명령어를 토큰(' ')으로 나눠 단어별로 저장한 문자열 배열 
  */
  void command(std::vector<std::string> vector_argv);
- 
-/**
- * @brief 명령어(command)에서 열거형 action을 반환하는 메소드
- * @param string command 명령어
- */
- backup::command::work get_work(const std::string command);
-
- /**
-  * @brief 명령어(command)에서 열거형 work을 반환하고 string str_work에 문자열 work을 저장하는 메소드
-  * @param string command 명령어
-  * @param string str_work work의 work의 문자열을 저장할 string 포인터
-  */
- backup::command::work get_work(const std::string command, std::string* const str_work);
 
  /**
  * @brief main의 인자로 입력된 명령어에서 열거형 work 을 반환하는 메소드
@@ -66,12 +47,6 @@ enum class path_option
  */
  backup::command::work get_work(std::vector<std::string> vector_argv);
  
-/**
- * @brief 명령어(connad)에서 root의 경로를 반환하는 메소드
- * @param string command 명령어
- * @param backup::command::path_option path_option 명령어/파일을
- */
- bfs::path get_root_path(const std::string command, backup::command::path_option path_option);
 
  /**
  * @brief main 인자로 입력된 명령어에서 root의 경로를 반환하는 메소드
@@ -80,23 +55,12 @@ enum class path_option
  */
  bfs::path get_root_path(std::vector<std::string> vector_argv);
  
-/**
- * @brief
- * @param string command 명령어
- */
- bfs::path get_dest_path(const std::string command, backup::command::path_option path_option);
 
  /**
  * @brief
  * @param string command 명령어
  */
  bfs::path get_dest_path(std::vector<std::string> vector_argv);
- 
-/**
- * @brief 옳바른 명령어인지 확인하는 메소드
- * @param string command 명령어
- */
- bool is_right(const std::string command);
 
  /**
  * @brief 옳바른 명령어인지 확인하는 메소드
